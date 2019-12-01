d789be0257635c3387bf059549d45d9196fe1077
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2019-12-01 17:46:35 -0300

    Add files via upload

--- AdventCode2019_Puzzle01.py
+++ AdventCode2019_Puzzle01.py
@@ -1,10 +1,12 @@
-
+# Collects the weight of each module and adds it to "modules_mass" list
 data_input = open("p01_input.txt", "r")
 modules_mass = []
 
 for line in data_input:
 	modules_mass.append(int(line))
 
+# Uses map to aplly "fuel_calculator" function to each module 
+# Saves results to "fuel_per_module" list and calculates total fuel required, then prints it.
 def fuel_calculator(module):
 	return (int(module/3)-2)
 
